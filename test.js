3a1df4b3979a55c03c9a0dadc02fe6454549cd80
skskimim <skskimim@example.com>
2024-07-24 16:02:15 +0900

    .

---
+++ test.js
@@ -0,0 +1,22 @@
+const prompt = require('prompt-sync')({sigint: true});
+
+let targetNumber = Math.floor(Math.random() * 100)+1;
+let guess = null;
+
+while (guess !== targetNumber) {
+    guess = parseInt(prompt("Guess a number between 1 and 100:"));
+    //만약에 타겟넘버보다 입력값이 크면 '그것보다 작아요!'라고 츨력해주자 (if)
+    //만약에 타겟넘버보다 입력값이 작으면 '그것보다는 커요!'라고 츨력해주자 (else if)
+    //만약에 타겟넘버보다 입력값이 같으면 '정답이에요!'라고 츨력하고 while을 종료 (else)
+    if (guess > targetNumber) {
+        console.log("그것보다 작아요!");
+    } else if (guess < targetNumber) {
+        console.log("그것보다는 커요!");
+    } else {
+        console.log("정답이에요!");
+        break;
+    }  
+}
+
+const number = Math.random()
+console.log(number);
